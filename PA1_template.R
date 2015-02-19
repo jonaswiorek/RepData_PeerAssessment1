90fc74695c68e801e2a7afc82f122bde15012974
jonaswiorek <jonas.wiorek@gmail.com>
2015-02-19 15:09:03 +0100

    Update of .R file

--- PA1_template.R
+++ PA1_template.R
@@ -17,10 +17,31 @@ abline(v=maxInterval, col = "dark red")
 
 length(which(is.na(activity$steps)))
 
-activityNoNa <- mutate(group_by(activity, interval), intervalSteps = mean(steps, na.rm = TRUE))
-activityNoNa <- mutate(activityNoNa, stepsNoNa = ifelse(is.na(steps), intervalSteps, steps))
-dailyStepsNoNa <- summarize(group_by(activityNoNa, date), steps = sum(stepsNoNa))
+#activityNoNa <- mutate(group_by(activity, interval), intervalSteps = floor(mean(steps, na.rm = TRUE)))
+#activityNoNa <- mutate(activityNoNa, steps = ifelse(is.na(steps), intervalSteps, steps))
+intervalSteps <- summarize(group_by(activity, interval), intervalSteps = floor(mean(steps, na.rm = TRUE)))
+activityNoNa <- mutate(activity, steps = ifelse(is.na(steps), intervalSteps$intervalSteps, steps))
+dailyStepsNoNa <- summarize(group_by(activityNoNa, date), steps = sum(steps))
 hist(dailyStepsNoNa$steps, breaks=20)
 mean(dailyStepsNoNa$steps)
 median(dailyStepsNoNa$steps)
+
+Sys.setlocale("LC_TIME","C") #Get weekdays in English in RStudio
+activityNoNa <- mutate(activityNoNa, day.of.week = as.factor(ifelse((weekdays(as.Date(activity$date))) %in% c("Saturday", "Sunday"), "weekend", "weekday")))
+intervalStepsWeek <- summarize(group_by(activityNoNa, interval, day.of.week), intervalSteps = floor(mean(steps)))
+
+
+qplot(interval, intervalSteps, 
+      data = intervalStepsWeek,
+      facets = day.of.week~.,
+      col = day.of.week,
+      geom = "line") 
+
+par(mfrow = c(2,1))
+with(intervalStepsWeek, plot(interval,intervalSteps, type= 'n'))
+with(subset(intervalStepsWeek, day.of.week == "weekend"), points(interval, intervalSteps, col = "dark blue", type = 'l'))
+with(intervalStepsWeek, plot(interval,intervalSteps, type= 'n'))
+with(subset(intervalStepsWeek, day.of.week == "weekday"), points(interval, intervalSteps, col = "dark red", type = 'l'))
+
+xyplot(intervalSteps ~ interval | day.of.week, data=intervalStepsWeek, type = "l", col ="dark blue", layout = c(1,2))
 
